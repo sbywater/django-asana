8d02192e46190d7392ea309ac40485b1354b44df
Steve Bywater <stephen.bywater@gmail.com>
2017-08-08 09:00:00 -0400

    Fixes webhook url

--- djasana/urls.py
+++ djasana/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 
 from .views import WebhookView
+from .settings import settings
 
 urlpatterns = [
-    url(r'^project/(?P<remote_id>\d+)/$', WebhookView.as_view(), name='djasana_webhook'),
+    url(
+        regex=r'^{0}project/(?P<remote_id>\d+)/$'.format(settings.DJASANA_WEBHOOK_URL),
+        view=WebhookView.as_view(),
+        name='djasana_webhook'
+    ),
 ]
