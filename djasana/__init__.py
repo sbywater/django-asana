deef25948f57667aae6dc44626dcc6b86c7b616c
Steve Bywater <stephen.bywater@gmail.com>
2017-12-22 12:30:49 -0500

    Version bump

--- djasana/__init__.py
+++ djasana/__init__.py
@@ -2,7 +2,7 @@
 # :copyright: (c) 2017 by Stephen Bywater.
 # :license:   MIT, see LICENSE for more details.
 
-VERSION = (0, 5, 18)
+VERSION = (0, 5, 19)
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __author__ = 'Steve Bywater'
 __contact__ = 'steve@regionalhelpwanted.com'
