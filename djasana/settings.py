03ef055bde359dff57fc7688e21068dfec8b915e
Steve Bywater <stephen.bywater@gmail.com>
2018-01-07 15:03:21 -0500

    Configures travis

--- djasana/settings.py
+++ djasana/settings.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-settings.DJASANA_WEBHOOK_URL = getattr(settings, 'DJASANA_WEBHOOK_URL', None)
-settings.DJASANA_WEBHOOK_PATTERN = getattr(
+DJASANA_WEBHOOK_URL = getattr(settings, 'DJASANA_WEBHOOK_URL', None)
+DJASANA_WEBHOOK_PATTERN = getattr(
     settings, 'DJASANA_WEBHOOK_PATTERN', r'^djasana/webhooks/')
-settings.ASANA_WORKSPACE = getattr(settings, 'ASANA_WORKSPACE', None)
+ASANA_WORKSPACE = getattr(settings, 'ASANA_WORKSPACE', None)
