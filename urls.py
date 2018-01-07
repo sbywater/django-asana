b8439fa29463f59c5628151f3ccdb958094a6e4e
Steve Bywater <stephen.bywater@gmail.com>
2018-01-07 13:00:14 -0500

    Tests use default settings

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
-from django.conf import settings
 from django.conf.urls import include, url
+from djasana import settings
 
 urlpatterns = [
     url(settings.DJASANA_WEBHOOK_PATTERN, include('djasana.urls')),
