787d6896b2bd525133423d201fcd500e3277882f
Steve Bywater <stephen.bywater@gmail.com>
2021-11-15 13:10:41 -0500

    Fixes deprecated import

--- djasana/models.py
+++ djasana/models.py
@@ -4,7 +4,7 @@ import logging
 from django.core.cache import cache
 from django.core.validators import MinLengthValidator
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .connect import client_connect
 
