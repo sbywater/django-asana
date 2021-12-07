386359e7b2da478053efdbc5528f06a8d7985253
Steve Bywater <stephen.bywater@gmail.com>
2021-12-07 15:34:23 -0500

    Updates urls for newer Django versions

--- djasana/urls.py
+++ djasana/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url, include
+from django.urls import include, re_path
 from djasana.settings import settings
 
 from .views import WebhookView
 
 urlpatterns = [
-    url(settings.DJASANA_WEBHOOK_PATTERN or r'^', include([
-        url(r'^project/(?P<remote_id>\d+)/$', view=WebhookView.as_view(), name='djasana_webhook'),
+    re_path(settings.DJASANA_WEBHOOK_PATTERN or r'^', include([
+        re_path(r'^project/(?P<remote_id>\d+)/$', view=WebhookView.as_view(), name='djasana_webhook'),
     ]))]
